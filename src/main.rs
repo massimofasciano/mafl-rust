b5194fda5d712f78cb5faff9f05b9acc287dd875
Massimo Fasciano <massimo@mafalitic.com>
2023-11-15 23:30:52 -0500

    go into repl mode if no program given

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::stdin, env::args};
+use std::{io::{stdin, BufRead, stdout, Write}, env::args};
 use mfel::Interpreter;
 use anyhow::{Result, anyhow};
 
@@ -8,16 +8,28 @@ fn main() -> Result<()> {
     if env.next().is_none() {
         return Err(anyhow!("no arg 0"));
     }
-    let source = if let Some(file) = env.next() {
-        std::fs::read_to_string(file)?
-    } else {
-        std::io::read_to_string(stdin())?
-    };
     let mut interpreter = Interpreter::new()?;
-    interpreter.set_env(env.map(|x|x.to_string()).collect());
-    let result = interpreter.run(&source)?;
-    println!();
-    println!("*** DEBUG INFO: full program evaluates to:");
-    interpreter.println(result)?;
+    if let Some(file) = env.next() {
+        // run the program
+        let source = std::fs::read_to_string(file)?;
+        interpreter.set_env(env.map(|x|x.to_string()).collect());
+        let result = interpreter.run(&source)?;
+        println!();
+        println!("*** DEBUG INFO: full program evaluates to:");
+        interpreter.println(result)?;
+    } else {
+        // REPL
+        let stdin = stdin();
+        loop {
+            print!("MFEL> "); stdout().flush()?;
+            let next_line = stdin.lock().lines().next();
+            if let Some(line_result) = next_line {
+                let result = interpreter.run(&line_result?)?;
+                interpreter.println(result)?;
+            } else {
+                break;
+            }
+        }
+    }
     Ok(())
 }
