4d930dbf47625f36b0a57fdefc76fcf8c4f2ed70
Massimo Fasciano <massimo@mafalitic.com>
2023-12-01 21:00:44 -0500

    update gitignore

---
+++ bin/dist.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+VER=1.1.3
+cp ../target/release/mafl mafl
+strip mafl
+cp ../target/release/mafl.exe mafl.exe 
+zip -9 mafl-${VER}-ubuntu-2204.zip mafl
+zip -9 mafl-${VER}-windows.zip mafl.exe 
